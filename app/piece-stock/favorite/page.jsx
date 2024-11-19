0b28dc5f3d60d44c40d3aad2bf66c238fdcc8ad8
newgamer11 <qfpotw4u81@naver.com>
2024-11-19 19:04:20 +0900

    Feat/StockWatchListSearch: piece-stock/favorite/page.jsx - 위시리스트조회 생성

--- app/piece-stock/favorite/page.jsx
+++ app/piece-stock/favorite/page.jsx
@@ -1,9 +1,36 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
 const FavoritePage = () => {
-    return (
-      <div>
-        선택종목 페이지
-      </div>
-    );
+  const [wishInfo, setWishInfo] = useState([]); // 조회한 위쉬 정보를 저장
+
+  useEffect(() => {
+    searchWishStocks();
+  }, [])
+
+  // 백엔드에서 GET 위시리스트 조회시, 위시리스트의 priority, stockName, stockPresentPrice, stockImage 를 갖고온다.
+  const searchWishStocks = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/stocks/select", {
+        method: 'GET',
+      });
+
+      if (!response.ok) {
+        throw new Error('값이 조회되지 않았습니다.');
+      }
+      const data = await response.json(); // 응답을 JSON으로 파싱
+      setWishInfo(data.data);
+    } catch (error) {
+      console.error('데이터 가져오기 오류:', error);
+    }
   }
-  
-  export default FavoritePage;
+
+
+  return (
+    <>
+      <p>위시리스트</p>
+    </>
+  )
+}
+export default FavoritePage;
