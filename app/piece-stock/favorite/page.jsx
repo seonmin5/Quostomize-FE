735bf6ead62fec02ca8a2cb84505e5730069666c
newgamer11 <qfpotw4u81@naver.com>
2024-11-20 09:24:57 +0900

    Feat/StockWatchList: peice-stock/favorite/page.jsx - 데이터 저장요청 생성

--- app/piece-stock/favorite/page.jsx
+++ app/piece-stock/favorite/page.jsx
@@ -1,9 +1,31 @@
+'use client'
+
 const FavoritePage = () => {
-    return (
-      <div>
-        선택종목 페이지
-      </div>
-    );
+
+  // 순서 변경 값
+  const orderInfo = [{
+    currentOrder: 1, // 현재 위치값
+    editOrder: 2 // 변경 위치값
+  }]
+
+  const switchStock = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/stocks/select/change-rank", {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',  // 요청 본문이 JSON임을 지정
+        },
+        body: JSON.stringify(orderInfo),
+      });
+      console.log(orderInfo)
+    } catch (error) {
+      console.error('데이터 가져오기 오류:', error);
+    }
   }
-  
-  export default FavoritePage;
+  return (
+    <>
+      <p>저장 요청</p>
+    </>
+  );
+}
+export default FavoritePage;
