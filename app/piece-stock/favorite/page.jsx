b44052795addb2486823d0848e1ae4a9bd6f940a
newgamer11 <qfpotw4u81@naver.com>
2024-11-20 15:08:52 +0900

    Refactor/StockWatchListSearch: 특정 카드아이디를 이용하여 조회

--- app/piece-stock/favorite/page.jsx
+++ app/piece-stock/favorite/page.jsx
@@ -3,16 +3,19 @@
 import { useEffect, useState } from "react";
 
 const FavoritePage = () => {
-  const [wishInfo, setWishInfo] = useState([]); // 조회한 위쉬 정보를 저장
+  const [wishInfo, setWishInfo] = useState([]) // 조회한 위쉬 정보를 저장
+  const param = new URLSearchParams() // 조회시 엔드포인트에 첨부할 내용 추가
 
+  // 처음 페이지 진입 시, 조회진행
   useEffect(() => {
+    param.append("cardId", cardId)
     searchWishStocks();
   }, [])
 
   // 백엔드에서 GET 위시리스트 조회시, 위시리스트의 priority, stockName, stockPresentPrice, stockImage 를 갖고온다.
   const searchWishStocks = async () => {
     try {
-      const response = await fetch("http://localhost:8080/api/stocks/select", {
+      const response = await fetch(`http://localhost:8080/api/stocks/select?${param}`, {
         method: 'GET',
       });
 
@@ -29,7 +32,7 @@ const FavoritePage = () => {
 
   return (
     <>
-      <p>위시리스트</p>
+      <p>위시리스트 조회</p>
     </>
   )
 }
